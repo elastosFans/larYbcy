e6c480e35599556a324f8ad0cc6a9c8e118e59d3
lijun2002 <1157447041@qq.com>
2017-09-10 22:07:29 +0800

    add

--- config/admin.php
+++ config/admin.php
@@ -5,17 +5,17 @@ return [
     /*
      * Laravel-admin name.
      */
-    'name'      => 'Laravel-admin',
+    'name'      => '亿保创元',
 
     /*
      * Logo in admin panel header.
      */
-    'logo'      => '<b>Laravel</b> admin',
+    'logo'      => '<b>亿保创元</b> admin',
 
     /*
      * Mini-logo in admin panel header.
      */
-    'logo-mini' => '<b>La</b>',
+    'logo-mini' => '<b>ybcy</b>',
 
     /*
      * Laravel-admin url prefix.
