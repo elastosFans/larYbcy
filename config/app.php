564a9021e545dc7f5da6834d93776bfc0b2d65fb
lijun2002 <1157447041@qq.com>
2017-09-14 02:04:20 +0800

    add

--- config/app.php
+++ config/app.php
@@ -51,8 +51,8 @@ return [
     |
     */
 
-    //'url' => env('APP_URL', 'https://localhost'),
-    'url' => env('APP_URL', 'https://isc.cie-china.com/admin'),
+    'url' => env('APP_URL', 'http://localhost'),
+    //'url' => env('APP_URL', 'https://isc.cie-china.com/admin'),
 
     /*
     |--------------------------------------------------------------------------
