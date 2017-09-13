97e84fabfc2765e947a4fd4d57e01167dca14c72
lijun2002 <1157447041@qq.com>
2017-09-14 05:36:17 +0800

    add

--- src/Form/Tools.php
+++ src/Form/Tools.php
@@ -72,13 +72,13 @@ EOT;
         $resource = $this->form->getResource($slice);
 
         $text = trans('admin::lang.list');
+        $resource = str_replace("http","https",$resource);
 
+        
         return <<<EOT
-<!--
 <div class="btn-group pull-right" style="margin-right: 10px">
     <a href="$resource" class="btn btn-sm btn-default"><i class="fa fa-list"></i>&nbsp;$text</a>
 </div>
--->
 EOT;
     }
 
