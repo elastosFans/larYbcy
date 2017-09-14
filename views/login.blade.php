3eaf83dbea2188fece6a24d921e522ce3fd3128b
lijun2002 <1157447041@qq.com>
2017-09-14 23:53:35 +0800

    add

--- views/login.blade.php
+++ views/login.blade.php
@@ -22,7 +22,7 @@
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <script src="js/respond.min.js"></script>
 
   <?php header('Access-Control-Allow-Origin:*'); ?>
   <![endif]-->
